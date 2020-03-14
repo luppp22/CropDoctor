9f7fdde56749048374866ae06ad4685a7eb6cbf1
4x10msv <1851727@tongji.edu.cn>
2020-03-14 12:58:03 +0800

    实现从图库选择图片功能

--- app/src/main/java/com/example/cropdoctor/MainActivity.java
+++ app/src/main/java/com/example/cropdoctor/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.cropdoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -13,13 +20,16 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int TAKE_PHOTO = 1;
+    public static final int TAKE_PHOTO      = 1;
+    public static final int CHOOSE_PHOTO    = 2;
     private Uri imageUri;
 
     @Override
@@ -29,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         //Log.d("MainActivity", "MainActivity begin!");
         Button btnCam = (Button) findViewById(R.id.button_camera);
         btnCam.setOnClickListener(new BtnCamClickListener());
+        Button btnPic = (Button) findViewById(R.id.button_picture);
+        btnPic.setOnClickListener(new BtnPicClickListener());
     }
 
     // 相机按钮监听器
@@ -57,18 +69,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 图库按钮监听器
+    class BtnPicClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            startActivityForResult(intent, CHOOSE_PHOTO);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
             case TAKE_PHOTO:
-                if (resultCode == RESULT_OK) { // 拍照成功，转到preview活动
+                if (resultCode == RESULT_OK) {
+                    PreviewActivity.activityStart(MainActivity.this, imageUri.toString());
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if(resultCode == RESULT_OK) {
+                    Uri contentUri = data.getData();
+                    imageUri = convertToFileUri(contentUri);
                     PreviewActivity.activityStart(MainActivity.this, imageUri.toString());
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    private Uri convertToFileUri(Uri contentUri) {
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(
+                contentUri,filePathColumn, null, null, null);
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String imagePath = cursor.getString(columnIndex);
+        cursor.close();
+        return Uri.fromFile(new File(imagePath));
     }
 }
